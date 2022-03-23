9d13fa6a1ec40d99010835b6b77ec36a8b0f6d72
Mustafa Durmuş <59168270+mstfdrms@users.noreply.github.com>
2022-03-23 23:44:56 +0300

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -3,7 +3,7 @@ def flatten(l):
     for e in l:
         if type(e) is list: 
             for item in e:
-                flatten(i)
+                flatten(l)
         elif:
             flatlist.append(e)
     return flatlist
