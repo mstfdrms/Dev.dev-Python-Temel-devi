33335f8c84af10e2a15ef599a16b518253c9d607
Mustafa Durmuş <59168270+mstfdrms@users.noreply.github.com>
2022-03-23 23:29:42 +0300

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -1,1 +1,9 @@
-
+flatlist = []
+def flatten(l):
+    for e in l:
+        if type(e)==list: 
+            for item in e:
+                flat(i)
+        else:
+            flatlist.append(e)
+    return flat_list
